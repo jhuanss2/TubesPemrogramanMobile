5d1c6d306fd86fb21472859399cea4224c1e9798
Jhuanss2 <Jhuanavryganda@students.usu.ac.id>
2022-10-23 23:03:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tubespemrogramanmobile/ActivityExample2.kt
@@ -0,0 +1,11 @@
+package layout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tubespemrogramanmobile.R
+
+class ActivityExample2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_2) }
+}
